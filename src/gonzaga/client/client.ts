3ad5cd7a615480ce0c411e337145dfefc5f6eb72
Gustavo Gonzaga <94642849+GonzagaGustavo@users.noreply.github.com>
2022-11-25 16:45:17 -0300

    Dando um tempo no projeto...

--- src/gonzaga/client/client.ts
+++ src/gonzaga/client/client.ts
@@ -4,7 +4,7 @@ RegisterCommand(
     const player = global.source; // use (global as any).source for Typescript
     const ped = GetPlayerPed(player);
     const [playerX, playerY, playerZ] = GetEntityCoords(ped);
-
+    console.log(GetPlayerPed(global.source));
     console.log(`${playerX}, ${playerY}, ${playerZ}`);
   },
   false
