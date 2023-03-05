a8b30168fc803df1e5ac3e001d6e003267a32229
Gustavo Gonzaga <94642849+GonzagaGustavo@users.noreply.github.com>
2023-03-05 13:40:17 -0300

    create-fivemworke-app done.

--- packages/create-fivemworke-app/index.ts
+++ packages/create-fivemworke-app/index.ts
@@ -5,13 +5,82 @@ import { Command } from "commander";
 import * as path from "path";
 import packageJsonLib from "./package.json";
 import * as fs from "fs-extra";
-import { exec } from "child_process";
+import { ChildProcess, exec } from "child_process";
 import { ncp } from "ncp";
+import os from "os";
+
+// async function install(packageManager: string = "yarn", root: string) {
+//   const packages = () =>
+//     new Promise((resolve, reject) => {
+//       const packagesToAdd = ["fivemworke", "@types/node", "typescript"];
+
+//       const installer = exec(
+//         `cd ${root} && ` +
+//           packageManager +
+//           " add" +
+//           packagesToAdd.map((pkg) => `${pkg} `)
+//       );
+//       installer.addListener("error", reject);
+//       installer.addListener("exit", resolve);
+//     });
+
+//   const devPackages = () =>
+//     new Promise((resolve, reject) => {
+//       const devPackagesToAdd = ["@citizenfx/client", "@citizenfx/server"];
+
+//       const installer = exec(
+//         `cd ${root} && ` +
+//           packageManager +
+//           " add -D " +
+//           devPackagesToAdd.map((pkg) => ` ${pkg}`)
+//       );
+//       installer.addListener("error", reject);
+//       installer.addListener("exit", resolve);
+//     });
+
+//   console.log("Installing the packages...");
+
+//   await packages();
+//   await devPackages();
+// }
+
+async function install(packageManager: string = "yarn", root: string) {
+  const packagesToAdd = ["fivemworke", "@types/node", "typescript"];
+  const devPackagesToAdd = ["@citizenfx/client", "@citizenfx/server"];
+
+  console.log(`Installing the dependencies using ${packageManager}`);
+
+  const installPackages = exec(
+    `cd "${root}" && ${packageManager} add ` + packagesToAdd.join(" ")
+  );
+
+  await promiseFromChildProcess(installPackages);
+
+  const installDevPackages = exec(
+    `cd "${root}" && ${packageManager} add -D ` + devPackagesToAdd.join(" ")
+  );
+  await promiseFromChildProcess(installDevPackages);
+}
+
+function promiseFromChildProcess(child: ChildProcess) {
+  return new Promise(function (resolve, reject) {
+    child.addListener("error", reject);
+    child.addListener("exit", resolve);
+  });
+}
+
+function template(root: string) {
+  ncp(`${__dirname}/src/template`, root, (err) =>
+    err ? console.log(err) : null
+  );
+}
 
 async function main() {
   let projectPath: string = "";
 
-  const program = new Command(packageJsonLib.name)
+  const program = new Command(packageJsonLib.name);
+
+  program
     .version(packageJsonLib.version)
     .arguments("<project-directory>")
     .usage(`${chalk.cyan("<project-directory>")} [options]`)
@@ -20,7 +89,8 @@ async function main() {
     })
     .parse(process.argv);
 
-  const root = path.resolve(projectPath);
+  const originalDirectory = process.cwd();
+  const root = path.join(originalDirectory, projectPath);
   const appName = path.basename(root);
 
   fs.ensureDirSync(projectPath);
@@ -34,45 +104,15 @@ async function main() {
       build: "fivemworke",
     },
   };
-  console.log(path.join(root, "/package.json"));
-  fs.writeFileSync(
+
+  await fs.promises.writeFile(
     path.join(root, "package.json"),
-    JSON.stringify(packageJson, null, 2)
+    JSON.stringify(packageJson, null, 2) + os.EOL
   );
 
-  //   await install(undefined, root);
+  await install(undefined, root);
   template(root);
+
+  console.log(chalk.green("Project created!"));
 }
 main();
-
-async function install(packageManager: string = "yarn", root: string) {
-  const packages = new Promise((resolve, reject) => {
-    const packagesToAdd = ["fivemworke", "@types/node", "typescript"];
-
-    const installer = exec(
-      `cd ${root} && ` +
-        packageManager +
-        " add" +
-        packagesToAdd.map((pkg) => `${pkg} `)
-    );
-    installer.addListener("error", reject);
-    installer.addListener("exit", resolve);
-  });
-
-  const devPackages = new Promise((resolve, reject) => {
-    const devPackagesToAdd = ["@citizenfx/client", "@citizenfx/server"];
-
-    const installer = exec(
-      packageManager + " add -D " + devPackagesToAdd.map((pkg) => ` ${pkg}`)
-    );
-    installer.addListener("error", reject);
-    installer.addListener("exit", resolve);
-  });
-
-  await packages;
-  await devPackages;
-}
-
-function template(root: string) {
-  ncp(`./src/template`, root, (err) => console.log(err));
-}
